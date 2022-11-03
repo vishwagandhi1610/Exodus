7910e3ab50a420405279ab21f782c2ca94ea5295
Chaitanya Patel <thechikkipatel@gmail.com>
2022-11-03 19:22:24 -0400

    adding animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D  body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        // Grab references for ridigbody and animator from object
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+
+        // Flip the direction when moving right and left
+        if (horizontalInput > 0.1f)
+        {
+            transform.localScale = Vector3.one;
+        } else if (horizontalInput < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // 
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        // Set animator parameters
+        anim.SetBool("walk", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
