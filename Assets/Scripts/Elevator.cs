cd0054713e0ed8aaf66b9606efa280ab6d22ac07
War-Keeper <87688584+War-Keeper@users.noreply.github.com>
2022-11-15 21:11:34 -0500

    working elevator

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,139 @@
+
+using System.Threading;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform elevatorswitch;
+    public Transform roof;
+    public Transform lobby;
+    public Transform basement;
+
+    public float speed;
+    bool level = true;
+    public int floor = 3;
+    public Vector2 diff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartElevator();
+        // Debug.Log(Vector2.Distance(player.position, elevatorswitch.position));
+        
+        
+    }
+
+    void StartElevator()
+    {
+        if (Vector2.Distance(player.position, elevatorswitch.position) < 0.5f && Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            floor = 3;
+
+            //diff = player.position - elevatorswitch.position;
+
+            if (transform.position.y == roof.position.y)
+            {
+                level = true;
+
+            }
+            else if (transform.position.y < roof.position.y)
+            {
+                level = false;
+                player.transform.parent = this.transform;
+
+            }
+
+            if (true)
+            {
+                Debug.Log(floor);
+                Debug.Log(level);
+            }
+
+        }
+
+        if (Vector2.Distance(player.position, elevatorswitch.position) < 0.5f && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            floor = 2;
+
+            //diff = player.position - elevatorswitch.position;
+
+            if (transform.position.y == lobby.position.y)
+            {
+                level = true;
+
+            }
+            else if (transform.position.y > lobby.position.y || transform.position.y < lobby.position.y)
+            {
+                level = false;
+                player.transform.parent = this.transform;
+            }
+
+        }
+
+        if (Vector2.Distance(player.position, elevatorswitch.position) < 0.5f && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            floor = 1;
+
+            //diff = player.position - elevatorswitch.position;
+
+            if (transform.position.y == basement.position.y)
+            {
+                level = true;
+
+            }
+            else if (transform.position.y > basement.position.y)
+            {
+                level = false;
+                player.transform.parent = this.transform;
+            }
+
+
+        }
+
+        if (!level && floor == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, basement.position, speed * Time.deltaTime);
+            //Vector2 s = diff + elevatorswitch.position;
+            //player.position = Vector2.MoveTowards(player.position, s, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, basement.position) < 0.01f)
+            {
+                player.transform.parent = null;
+            }
+
+        }
+
+        if (!level && floor == 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, lobby.position, speed * Time.deltaTime);
+            //Vector2 s = diff + elevatorswitch.position;
+            //player.position = Vector2.MoveTowards(player.position, s, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, lobby.position) < 0.01f)
+            {
+                player.transform.parent = null;
+            }
+        }
+
+        if (!level && floor == 3)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, roof.position, speed * Time.deltaTime);
+            //Vector2 s = diff + elevatorswitch.position;
+            //player.position = Vector2.MoveTowards(player.position, s, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, roof.position) < 0.01f)
+            {
+                player.transform.parent = null;
+            }
+        }
+
+        
+
+
+    }
+}
